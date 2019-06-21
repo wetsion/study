28198c373369dcaab8fbaf8d68aa914f83282678
魏鑫 <weixin@dxy.cn>
2019-06-21 19:22:27 +0800

    [add] 增加

--- src/main/java/com/wetsion/study/async/AsyncController.java
+++ src/main/java/com/wetsion/study/async/AsyncController.java
@@ -1,9 +1,12 @@
 package com.wetsion.study.async;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
@@ -14,7 +17,10 @@ import java.util.concurrent.Future;
  * @date 2019/6/12 10:46 AM
  */
 @RestController
+@Slf4j
 public class AsyncController {
+
+    private List<Future<String>> futures = new ArrayList<>();
 
     @Autowired
     AsyncService asyncService;
@@ -27,7 +33,21 @@ public class AsyncController {
     @GetMapping("/async/test3")
     public void testResultAsync() throws ExecutionException, InterruptedException {
         Future<String> future = asyncService.testFuture();
+        futures.add(future);
         asyncService.test();
-        System.out.println(future.get());
+//        System.out.println(future.get());
+    }
+
+    @GetMapping("/async/test3/get")
+    public void testResultAsyncGet() {
+        futures.forEach(f -> {
+            try {
+                log.info(f.get());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+        });
     }
 }
